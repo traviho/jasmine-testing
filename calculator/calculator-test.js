7f51b8a870060aab22ca966c33c965a51d0ebedf
traviho <thdarkblue@hotmail.com>
2023-12-19 17:41:00 -0500

    write tests for monthly payment function

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,6 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const amount = 500000;
+  const years = 10;
+  const rate = 0.06;
+  const values = {amount, years, rate};
+  const monthlyPayment = calculateMonthlyPayment(values);
+  expect(monthlyPayment).toEqual(5551.03);
 });
 
 

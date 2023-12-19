6db23c04eddac0ca3b6ca6d7bfe6f36e6e98589e
traviho <thdarkblue@hotmail.com>
2023-12-19 17:40:47 -0500

    calculate monthly payment and update ui

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,34 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").value = 0;
+  document.getElementById("loan-years").value = 0;
+  document.getElementById("loan-rate").value = 0;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const monthlyPayment = calculateMonthlyPayment(values).toString();
+  updateMonthly(monthlyPayment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const {amount, years, rate} =  values;
+  const i = rate / 12;
+  const n = years * 12;
+  var monthlyPayment = (amount * i ) / (1 - Math.pow(1 + i, -n));
+  monthlyPayment = Math.round(monthlyPayment * 100) / 100
+  return monthlyPayment;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const span = document.getElementById("monthly-payment");
+  span.textContent = monthly;
 }
